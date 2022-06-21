8849d980cabfb7cf394142849f9ac11cde0cc8ec
Tim Leach <timleach635@gmail.com>
2022-06-21 23:27:09 +0100

    Make SyllablePattern implement IList<Stress>

--- Music/Music/Lyrics/SyllablePattern.cs
+++ Music/Music/Lyrics/SyllablePattern.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Music.Lyrics
 {
-    public class SyllablePattern
+    public class SyllablePattern : IList<Stress>
     {
         public IList<Stress> Syllables { get; }
+        public int Count => Syllables.Count;
+
+        public bool IsReadOnly => Syllables.IsReadOnly;
 
         public SyllablePattern(IList<Stress> syllables)
         {
@@ -35,5 +39,28 @@ namespace Music.Lyrics
                 }
             }
         }
+
+        public Stress this[int index]
+        {
+            get
+            {
+                return Syllables[index];
+            }
+            set
+            {
+                Syllables[index] = value;
+            }
+        }
+
+        public int IndexOf(Stress stress) => Syllables.IndexOf(stress);
+        public void Insert(int index, Stress stress) => Syllables.Insert(index, stress);
+        public void RemoveAt(int index) => Syllables.RemoveAt(index);
+        public void Add(Stress item) => Syllables.Add(item);
+        public void Clear() => Syllables.Clear();
+        public bool Contains(Stress item) => Syllables.Contains(item);
+        public void CopyTo(Stress[] array, int arrayIndex) => Syllables.CopyTo(array, arrayIndex);
+        public bool Remove(Stress item) => Syllables.Remove(item);
+        public IEnumerator<Stress> GetEnumerator() => Syllables.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)Syllables).GetEnumerator();
     }
 }
